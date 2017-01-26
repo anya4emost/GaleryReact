c951d4f9e1c165c6527e9a8a572768e832612977
Anna <kashlatch@gmail.com>
2017-01-26 20:02:35 +0300

    refactoring - to react Component

--- src/app.jsx
+++ src/app.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Gallery} from './components/gallery/GalleryComponent';
 
-
+let massivchik = ["src/images/1.jpg", "src/images/2.jpg", "src/images/3.jpg", "src/images/4.jpg", "src/images/5.jpg", "src/images/6.jpg", "src/images/7.jpg", "src/images/8.jpg", "src/images/9.jpg"];
 ReactDOM.render(
-    <Gallery/>,
+    <Gallery images={massivchik} currentIndex={4} slideSpeed={3} canOpenPopup={false}/>,
     document.getElementById("app")
 );
--- src/components/gallery/GalleryComponent.jsx
+++ src/components/gallery/GalleryComponent.jsx
@@ -5,34 +5,27 @@ export class Gallery extends React.Component {
 
     constructor(props) {
         super(props);
-        this.images = ["src/images/1.jpg", "src/images/2.jpg", "src/images/3.jpg", "src/images/4.jpg", "src/images/5.jpg", "src/images/6.jpg", "src/images/7.jpg", "src/images/8.jpg", "src/images/9.jpg"];
-        this.slideSpeed = 3;
-        this.canOpenPopup = true;
-        this.currentImageIndex = 4;
     }
 
     render() {
+        
+        let photoIcons = this.props.images.map(function(photo){
+          return  (<li style={{backgroundImage: `url(${photo})`}}></li>);
+        });
+
         return (
             <div>
                 <div className="bigPhoto-container">
                     <img className="arrow" src="../src/images/arrow-left.png"/>
                     <div className="bigPhoto">
-                        <img src={this.images[this.currentImageIndex]}/>
+                        <img src={this.props.images[this.props.currentIndex]}/>
                     </div>
                     <img className="arrow" src="../src/images/arrow-right.png"/>
                 </div>
                 <div className="miniPhoto-container">
                     <img className="arrow" src="../src/images/arrow-left.png"/>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        {photoIcons}
                     </ul>
                     <img className="arrow" src="../src/images/arrow-right.png"/>
                 </div>
